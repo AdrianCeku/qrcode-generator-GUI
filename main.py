9ef14a65619c9840fc15034fc7a0d4940b32df3f
AdrianCeku <cekuadrian4@gmail.com>
2023-04-19 22:05:45 +0200

    Added all labels

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import qrcode.image.svg
 import tkinter
 import customtkinter 
 
+# Generates the QR Code and saves it. Gets called when you press on "Generate" 
 def create_qr():
     qr = qrcode.QRCode(
             version=version_slider.get(),
@@ -18,7 +19,7 @@ def create_qr():
 
     img.save(f"QR-Codes/{filename_box.get()}{filetype_box.get()}")
 
-
+# Returns selected Error Correction Level. Gets called when Generating a QR Code
 def get_error_correction():
     if error_corection_box.get()=="L (7%)":
         return qrcode.constants.ERROR_CORRECT_L
@@ -30,6 +31,7 @@ def get_error_correction():
         return qrcode.constants.ERROR_CORRECT_H
     else: return qrcode.constants.ERROR_CORRECT_M
 
+# Updates slider values to represent current value. Gets called whenever a slider is moved
 def update_slider_labels(_):
     version_slider_label.configure(text=f"Version ({int(version_slider.get())})")
     boxsize_slider_label.configure(text=f"Size ({int(boxsize_slider.get())})")
@@ -54,7 +56,7 @@ generate_button = customtkinter.CTkButton(master=root,
                                 command=create_qr,)
 generate_button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
-# Filename Entrybox
+# Filename Entrybox + Label
 filename_box =customtkinter.CTkEntry(master=root,
                             #placeholder_text="Filename",
                             width=450,
@@ -71,7 +73,7 @@ filename_box_label = customtkinter.CTkLabel(master=root,
                                 fg_color="transparent")
 filename_box_label.place(relx=0.175, rely=0.060, anchor=tkinter.CENTER)
 
-# Content Textbox
+# Content Textbox + Label
 content_box = customtkinter.CTkTextbox(master=root,
                             width=450,
                             height=200,
@@ -87,7 +89,7 @@ content_box_label = customtkinter.CTkLabel(master=root,
                                 fg_color="transparent")
 content_box_label.place(relx=0.170, rely=0.15, anchor=tkinter.CENTER)
 
-# Advanced Options Frame
+# Advanced Options Frame + Label
 advanced_options = customtkinter.CTkFrame(master=root,
                             width=450,
                             height=150,
@@ -103,43 +105,71 @@ advanced_options_label = customtkinter.CTkLabel(master=root,
                                 fg_color="transparent")
 advanced_options_label.place(relx=0.22, rely=0.55, anchor=tkinter.CENTER)
 
-# Filetype Combobox
+# Filetype Combobox + Label
 filetype_box = customtkinter.CTkComboBox(master=advanced_options,
                                     width=70,
                                     height=20,
                                     values=[".png",".jpg",".svg",".webp"],
                                     )
-filetype_box.place(relx=0.2, rely=0.2, anchor=tkinter.CENTER)
-filetype_box.set(".png")  # set initial value
+filetype_box.place(relx=0.2, rely=0.27, anchor=tkinter.CENTER)
+filetype_box.set(".png")  # Sets initial value
 
-# Errorcorrection Combobox
+filetype_box_label = customtkinter.CTkLabel(master=advanced_options,
+                                text="File Extension",
+                                height=20,
+                                corner_radius=8,
+                                fg_color="transparent")
+filetype_box_label.place(relx=0.2, rely=0.12, anchor=tkinter.CENTER)
+
+# Errorcorrection Combobox + Label
 error_corection_box = customtkinter.CTkComboBox(master=advanced_options,
                                     width=70,
                                     height=20,
                                     values=["L (7%)","M (15%)","Q (25%)","H (30%)"],
                                     )
-error_corection_box.place(relx=0.4, rely=0.2, anchor=tkinter.CENTER)
-error_corection_box.set("M (15%)")  # set initial value
+error_corection_box.place(relx=0.4, rely=0.27, anchor=tkinter.CENTER)
+error_corection_box.set("M (15%)")  # Sets initial value
 
-# Color Combobox
+filetype_box_label = customtkinter.CTkLabel(master=advanced_options,
+                                text="Error Crrection",
+                                height=20,
+                                corner_radius=8,
+                                fg_color="transparent")
+filetype_box_label.place(relx=0.4, rely=0.12, anchor=tkinter.CENTER)
+
+# Color Combobox # Label
 fill_color_box = customtkinter.CTkComboBox(master=advanced_options,
                                     width=70,
                                     height=20,
                                     values=["black","white","red","green", "blue", "yellow", "orange", "pink", "purple"],
                                     )
-fill_color_box.place(relx=0.6, rely=0.2, anchor=tkinter.CENTER)
-fill_color_box.set("black")  # set initial value
+fill_color_box.place(relx=0.6, rely=0.27, anchor=tkinter.CENTER)
+fill_color_box.set("black")  # Sets initial value
 
-# Backgroundcolor Combobox
+filetype_box_label = customtkinter.CTkLabel(master=advanced_options,
+                                text="Color",
+                                height=20,
+                                corner_radius=8,
+                                fg_color="transparent")
+filetype_box_label.place(relx=0.6, rely=0.12, anchor=tkinter.CENTER)
+
+# Backgroundcolor Combobox + Slider
 background_color_box = customtkinter.CTkComboBox(master=advanced_options,
                                     width=70,
                                     height=20,
                                     values=["black","white","red","green", "blue", "yellow", "orange", "pink", "purple"],
                                     )
-background_color_box.place(relx=0.8, rely=0.2, anchor=tkinter.CENTER)
-background_color_box.set("white")  # set initial value
+background_color_box.place(relx=0.8, rely=0.27, anchor=tkinter.CENTER)
+background_color_box.set("white")  # Sets initial value
 
-# Version Slider
+filetype_box_label = customtkinter.CTkLabel(master=advanced_options,
+                                text="Background Color",
+                                height=20,
+                                corner_radius=8,
+                                fg_color="transparent")
+filetype_box_label.place(relx=0.8, rely=0.12, anchor=tkinter.CENTER)
+
+# Version Slider + Label
 version_slider = customtkinter.CTkSlider(master=advanced_options,
                                         width=320,
                                         height=20,
@@ -147,17 +177,17 @@ version_slider = customtkinter.CTkSlider(master=advanced_options,
                                         to=40,
                                         number_of_steps=40,
                                         command=update_slider_labels)
-version_slider.place(relx=0.6, rely=0.4, anchor=tkinter.CENTER)
-version_slider.set(1)
+version_slider.place(relx=0.6, rely=0.47, anchor=tkinter.CENTER)
+version_slider.set(1) # Sets initial value
 
 version_slider_label = customtkinter.CTkLabel(master=advanced_options,
                                 text=f"Version ({int(version_slider.get())})",
                                 height=20,
                                 corner_radius=8,
                                 fg_color="transparent")
-version_slider_label.place(relx=0.125, rely=0.4, anchor=tkinter.CENTER)
+version_slider_label.place(relx=0.125, rely=0.47, anchor=tkinter.CENTER)
 
-# Boxsize Slider
+# Boxsize Slider + Label
 boxsize_slider = customtkinter.CTkSlider(master=advanced_options,
                                         width=320,
                                         height=20,
@@ -165,8 +195,8 @@ boxsize_slider = customtkinter.CTkSlider(master=advanced_options,
                                         to=100,
                                         number_of_steps=100,
                                         command=update_slider_labels)
-boxsize_slider.place(relx=0.6, rely=0.6, anchor=tkinter.CENTER)
-boxsize_slider.set(11)
+boxsize_slider.place(relx=0.6, rely=0.67, anchor=tkinter.CENTER)
+boxsize_slider.set(11) # Sets initial value. Set to 11 instead of 10 because of floating point errors
 
 boxsize_slider_label = customtkinter.CTkLabel(master=advanced_options,
                                 text=f"Size ({int(boxsize_slider.get())})",
@@ -174,9 +204,9 @@ boxsize_slider_label = customtkinter.CTkLabel(master=advanced_options,
                                 corner_radius=8,
                                 fg_color="transparent",
                                 )
-boxsize_slider_label.place(relx=0.125, rely=0.6, anchor=tkinter.CENTER)
+boxsize_slider_label.place(relx=0.125, rely=0.67, anchor=tkinter.CENTER)
 
-# Border Slider
+# Border Slider + Label
 border_slider = customtkinter.CTkSlider(master=advanced_options,
                                         width=320,
                                         height=20,
@@ -184,17 +214,17 @@ border_slider = customtkinter.CTkSlider(master=advanced_options,
                                         to=100,
                                         number_of_steps=96,
                                         command=update_slider_labels)
-border_slider.place(relx=0.6, rely=0.8, anchor=tkinter.CENTER)
-border_slider.set(4)
+border_slider.place(relx=0.6, rely=0.87, anchor=tkinter.CENTER)
+border_slider.set(4) # Sets initial value
 
 border_slider_label = customtkinter.CTkLabel(master=advanced_options,
                                 text=f"Border Size ({int(border_slider.get())})",
                                 height=20,
                                 corner_radius=8,
                                 fg_color="transparent")
-border_slider_label.place(relx=0.125, rely=0.8, anchor=tkinter.CENTER)
+border_slider_label.place(relx=0.125, rely=0.87, anchor=tkinter.CENTER)
 
 
-
+# Starts the program
 root.mainloop()
 
